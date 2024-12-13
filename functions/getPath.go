4cca9248f5294d95b814b4dd6b1d1c009115ee02
idmossab <idmossab@gmail.com>
2024-12-13 20:53:03 +0100

    update 1 getpaths

--- functions/getPath.go
+++ functions/getPath.go
@@ -1,21 +1,19 @@
 package lemin
 
-func GetPaths(af []Tunnel, Start string, End string, path []string) {
+func  GetPaths(af []Tunnel, Start string, End string, path []string) {
 	var paths = &Paths
-	if len(*paths) == 5000 {
+	path = append(path, Start)
+	if Start == End {
+		cpy := make([]string, len(path))
+		copy(cpy, path)
+		*paths = append(*paths, cpy)
 		return
 	}
-	path = append(path, Start)
-	for h := 0; h < len(af); h++ {
-		if Start == End {
-			cpy := make([]string, len(path))
-			copy(cpy, path)
-			*paths = append(*paths, cpy)
-			return
-		} else if Start == af[h].From && !Contains(path, af[h].To) {
-			GetPaths(af, af[h].To, End, path)
-		} else if Start == af[h].To && !Contains(path, af[h].From) {
-			GetPaths(af, af[h].From, End, path)
+	for _, tunnel := range af {
+		if Start == tunnel.From && !Contains(path, tunnel.To) {
+			GetPaths(af, tunnel.To, End, path,)
+		} else if Start == tunnel.To && !Contains(path, tunnel.From) {
+			GetPaths(af, tunnel.From, End, path)
 		}
 	}
 }
