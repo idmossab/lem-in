9f371255fceaa7bf86cf9da8de17ba9b36e8acf3
idmossab <idmossab@gmail.com>
2024-12-14 17:54:12 +0100

    update getpath

--- functions/getPaths.go
+++ functions/getPaths.go
@@ -4,19 +4,20 @@ func  GetPaths(tunnels []Tunnel, Start string, End string, path []string) {
 	var paths = &Paths
 	path = append(path, Start)
 	if Start == End {
-		cpy := make([]string, len(path))
-		copy(cpy, path)
-		*paths = append(*paths, cpy)
+		// cpy := make([]string, len(path))
+		// copy(cpy, path)
+		*paths = append(*paths, path)
 		return
 	}
 	for _, tunnel := range tunnels {
 		if Start == tunnel.From && !Contains(path, tunnel.To) {
-			GetPaths(tunnels, tunnel.To, End, path,)
+			GetPaths(tunnels, tunnel.To, End, path)
 		} else if Start == tunnel.To && !Contains(path, tunnel.From) {
 			GetPaths(tunnels, tunnel.From, End, path)
 		}
 	}
 }
+
 func Contains(arr []string, str string) bool {
 	for _, v := range arr {
 		if v == str {
