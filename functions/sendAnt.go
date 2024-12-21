2b3e871f44b34ee378a78640e35f587807f30796
idmossab <idmossab@gmail.com>
2024-12-21 21:37:54 +0100

    addaptation nbr ant

--- functions/sendAnt.go
+++ functions/sendAnt.go
@@ -2,22 +2,34 @@ package lemin
 
 import "fmt"
 
-func SendAnt(groupes [][][]string, ant int) {
-    bestGroup := groupes[0] 
-    lenbestGroup := make([]int, len(bestGroup)) 
+func SendAnt(groups [][][]string, ants int) {
+    bestGroup := groups[0]
+    lenbestGroup := make([]int, len(bestGroup))
+    pathLengths := make([]int, len(bestGroup))
 
-    for ant > 0 { 
-        shortestPath := 0 
-        for i := 1; i < len(lenbestGroup); i++ {
-            if lenbestGroup[i] < lenbestGroup[shortestPath] {
-                shortestPath = i 
-            }
-        }
-        lenbestGroup[shortestPath]++
-        ant-- 
+    // Calculate the length of each path
+    for i, path := range bestGroup {
+        pathLengths[i] = len(path)
     }
 
-    for i, length := range lenbestGroup {
-        fmt.Printf("Path %d: %d ants\n", i+1, length)
+    // Distribute ants, prioritizing shorter paths
+    for ants > 0 {
+        shortestPath := 0
+
+        // Find the shortest path in terms of the current load
+        for i := 1; i < len(lenbestGroup); i++ {
+            if lenbestGroup[i]+pathLengths[i] < lenbestGroup[shortestPath]+pathLengths[shortestPath] {
+                shortestPath = i
+            }
+        }
+
+        // Assign an ant to the shortest path
+        lenbestGroup[shortestPath]++
+        ants--
+    }
+
+    // Print the result
+    for i, count := range lenbestGroup {
+        fmt.Printf("Path %d: %d ants\n", i+1, count)
     }
 }
