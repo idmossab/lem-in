c7bdb1edbf25a44976d8d7b58db6063a25b10692
Mossab Idbenkellal <idmossab@gmail.com>
2024-12-21 12:42:06 +0100

    update groupes

--- functions/groupes.go
+++ functions/groupes.go
@@ -14,23 +14,20 @@ func GroupPaths(paths [][]string) [][][]string {
 			continue // Skip already grouped paths
 		}
 
-		currentGroup := [][]string{path} // Start a new group with the current path
+		currentGroup := [][]string{path} 
 		used[i] = true
 
-		// Try to add other compatible paths to the group
 		for j, otherPath := range paths {
 			if used[j] {
 				continue // Skip already used paths
 			}
 
-			// Check if adding `otherPath` to the group maintains compatibility
 			if isCompatible(currentGroup, otherPath) {
 				currentGroup = append(currentGroup, otherPath)
 				used[j] = true
 			}
 		}
 
-		// Append the formed group to the list of groups if it's unique
 		if !groupExists(groups, currentGroup) {
 			groups = append(groups, currentGroup)
 		}
@@ -43,8 +40,7 @@ func GroupPaths(paths [][]string) [][][]string {
 // Helper function to check if a path is compatible with a group
 func isCompatible(group [][]string, path []string) bool {
 	for _, groupPath := range group {
-		// If the new path has common intermediate nodes with any path in the group, it's not compatible
-		if hasCommonIntermediateNodes(groupPath, path) {
+		if hasCommonRooms(groupPath, path) {
 			return false
 		}
 	}
@@ -52,19 +48,19 @@ func isCompatible(group [][]string, path []string) bool {
 }
 
 // Check if two paths share room (excluding "start" and "end")
-func hasCommonIntermediateNodes(path1, path2 []string) bool {
-	nodes1 := path1[1 : len(path1)-1] // Exclude "start" and "end"
-	nodes2 := path2[1 : len(path2)-1] // Exclude "start" and "end"
+func hasCommonRooms(path1, path2 []string) bool {
+	nodes1 := path1[1 : len(path1)-1] 
+	nodes2 := path2[1 : len(path2)-1]
 
 	for _, node := range nodes1 {
 		if slices.Contains(nodes2, node) {
-			return true // Common intermediate node found
+			return true 
 		}
 	}
 	return false
 }
 
-// Check if a group already exists in the final groups
+// Check if a group already exists
 func groupExists(groups [][][]string, newGroup [][]string) bool {
 	for _, group := range groups {
 		if areGroupsEqual(group, newGroup) {
@@ -80,7 +76,6 @@ func areGroupsEqual(group1, group2 [][]string) bool {
 		return false
 	}
 
-	// Ensure all paths in one group are present in the other
 	for _, path1 := range group1 {
 		found := false
 		for _, path2 := range group2 {
