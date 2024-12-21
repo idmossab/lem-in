4f739495bdff6abf498748aca50742b3c9fc1eb7
Mossab Idbenkellal <idmossab@gmail.com>
2024-12-21 18:02:34 +0100

    send ant v_2

--- functions/sendAnt.go
+++ functions/sendAnt.go
@@ -3,23 +3,21 @@ package lemin
 import "fmt"
 
 func SendAnt(groupes [][][]string, ant int) {
-	bestGroup := groupes[0]
-	lenbestGroup:=make([][]int,len(bestGroup))
-	//lenbestGroup[1] = append(lenbestGroup[1], "1")
-	for i,_:=range bestGroup{
-		pth:=i
-		fmt.Println("pth:::",pth)
-		for j,_:=range lenbestGroup{
-			antlen:=j
-			if pth>=antlen && ant >0{
-				lenbestGroup = append(lenbestGroup,lenbestGroup...)
-				ant--
-			}
-		}
-	}
-	
-	fmt.Println("Best Group:", lenbestGroup)
-	fmt.Println("Best Group:", bestGroup)
+    bestGroup := groupes[0] 
+    lenbestGroup := make([]int, len(bestGroup)) 
 
-	
+    for ant > 0 { 
+        shortestPath := 0 
+        for i := 1; i < len(lenbestGroup); i++ {
+            if lenbestGroup[i] < lenbestGroup[shortestPath] {
+                shortestPath = i 
+            }
+        }
+        lenbestGroup[shortestPath]++
+        ant-- 
+    }
+
+    for i, length := range lenbestGroup {
+        fmt.Printf("Path %d: %d ants\n", i+1, length)
+    }
 }
