258fc1e5c2e0ca034f4da1e0174bbd165be0301a
Mossab Idbenkellal <idmossab@gmail.com>
2024-12-21 17:20:49 +0100

    send ant v_1

--- functions/sendAnt.go
+++ functions/sendAnt.go
@@ -4,5 +4,22 @@ import "fmt"
 
 func SendAnt(groupes [][][]string, ant int) {
 	bestGroup := groupes[0]
-	fmt.Println("Best Group:", bestGroup) // Example output to verify
+	lenbestGroup:=make([][]int,len(bestGroup))
+	//lenbestGroup[1] = append(lenbestGroup[1], "1")
+	for i,_:=range bestGroup{
+		pth:=i
+		fmt.Println("pth:::",pth)
+		for j,_:=range lenbestGroup{
+			antlen:=j
+			if pth>=antlen && ant >0{
+				lenbestGroup = append(lenbestGroup,lenbestGroup...)
+				ant--
+			}
+		}
+	}
+	
+	fmt.Println("Best Group:", lenbestGroup)
+	fmt.Println("Best Group:", bestGroup)
+
+	
 }
